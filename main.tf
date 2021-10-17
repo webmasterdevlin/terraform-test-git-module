6f0235b10651b1ed0b51b813691ba65baedfa577
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-17 22:28:35 +0200

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 module "name_of_platform" {
-  source               = "./platform/production"
+  source = "./platform/production"
+  # OR
+  # source             = "github.com/webmasterdevlin/terraform-test-git-module.git//platform/production"
+  # don't forget to add the .git suffix to the repo name plus double slashes
+
   platform_rg_name     = var.platform_rg_name
   platform_rg_location = var.platform_rg_location
 }
